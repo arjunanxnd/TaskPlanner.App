71e56fb829f7f29132a184daa09b85b0c33525d6
digaantdogra <dogra057@gmail.com>
2023-08-08 09:48:27 -0400

    Modification

--- Business Logic/Assignment.cs
+++ Business Logic/Assignment.cs
@@ -6,17 +6,17 @@ using System.Threading.Tasks;
 
 namespace TaskPlanner.Business_Logic
 {
-    class Assignment
+    class Assignment : Assessment
     {
         private List<Assignment> _assignments = new List<Assignment>();
         public List<Assignment> Assignments { get { return _assignments; } }
 
-        public override void AddAssessment(Assignment assignment)
+        public void AddAssesment(Assignment assignment)
         {
             _assignments.Add(assignment);
         }
 
-        public override void DeleteAssessment(Assignment assignment) //have to make more stuff
+        public void DeleteAssessment(Assignment assignment) //have to make more stuff
         {
             foreach(Assignment var in _assignments)
             {
