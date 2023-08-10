efc66740a780936ceecaa9e53a03d87567d68860
digaantdogra <dogra057@gmail.com>
2023-08-09 22:06:11 -0400

    Login page in progress

--- Pages/LoginPage.xaml.cs
+++ Pages/LoginPage.xaml.cs
@@ -3,21 +3,42 @@ using TaskPlanner.Business_Logic;
 
 public partial class LoginPage : ContentPage
 {
-    
+    UserRepository _repository;
 	public LoginPage()
 	{
 		InitializeComponent();
-
+        _repository = new UserRepository();
     }
 
     private async void LoginSucsflBtn_Clicked(System.Object sender, System.EventArgs e)
     {
-        await Shell.Current.GoToAsync("//Home");
+        try
+        {
+            
+            string uName = UserNameEntry.Text;
+            string pass = PasswordEntry.Text;
+            if (uName == null && pass == null)
+                throw new Exception("Please Enter full details to proceed");
+
+            foreach (User user in _repository.Users)
+            {
+                if (user.UserName == uName && user.Password == pass)
+                {
+                    await DisplayAlert("Welcome", $"{user.FirstAndLastName}", "OK");
+                    await Shell.Current.GoToAsync("///Home");
+                    break;
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            DisplayAlert("Error", $"{ex.Message}", "OK");
+        }
     }
 
     private async void SignUpBtn_Clicked(System.Object sender, System.EventArgs e)
     {
-        SignUpPage signUpPage = new SignUpPage();
+        SignUpPage signUpPage = new SignUpPage(_repository);
         await Navigation.PushAsync(signUpPage);
     }
 }
